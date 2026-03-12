f6d75ac7c6c10c968c48174f4074d4ee7acd25ac
Jon Van Steen <jvansteen1@gmail.com>
2026-03-12 16:30:26 -0400

    fix linux build

--- Package.swift
+++ Package.swift
@@ -56,6 +56,7 @@ targets += [
         cSettings: [
             .unsafeFlags([
                 "-I/usr/include/glib-2.0",
+                "-I/usr/lib/x86_64-linux-gnu/glib-2.0/include",
                 "-I/usr/lib/glib-2.0/include",
                 "-I/usr/include/libsecret-1",
                 "-I/usr/include/gio-unix-2.0",
